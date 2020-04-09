20b17fdce0c6c69ca4a0e083c1e9a7c452cc6ca7
IAmAPersson <iamaperson620@gmail.com>
2020-04-08 21:08:10 -0500

    working on disjoint sets

--- prog3a.pl
+++ prog3a.pl
@@ -1,2 +1,12 @@
+/* Phil Lane
+   4/8/2020
+   CS 424 Program 3
+   Tested with SWI-Prolog v7.6.4
+*/
+
 sumList([], 0).
 sumList([X|Tail], Z) :- sumList(Tail, Y), Z is X + Y.
+
+isIn(X, [A|Tail]) :- X is A; isIn(X, Tail).
+intersection(A, B, X) :- ((isIn(T, A), isIn(T, B)) -> isIn(T, X)).
+disjoint(A, B) :- intersection(A, B, C), not(isIn(X, C)).
